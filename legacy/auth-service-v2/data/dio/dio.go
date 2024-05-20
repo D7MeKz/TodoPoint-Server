1256e6e0e106f35301e331e3fc7ea5fa61b85b37
D7MeKz <damekyung98@gmail.com>
2024-05-20 10:29:00 +0900

    [Chore] Arrange folder(TDP-47)

--- legacy/auth-service-v2/data/dio/dio.go
+++
@@ -1,7 +0,0 @@
-package dio
-
-// Credential is a struct that contains user credential httpdata.
-type Credential struct {
-	Email    string
-	Password string
-}
--- mobile/android/app/src/main/kotlin/com/todopoint/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.todopoint.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
