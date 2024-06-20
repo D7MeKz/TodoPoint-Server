c8d88c126ed79187e327505453df3418ed922b2a
D7MeKz <damekyung98@gmail.com>
2024-06-20 17:11:21 +0900

    [Feat] Change Init workspace scripts

--- scripts/init_workspace.sh
+++ scripts/init_workspace.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+PACKAGE_FILE="workspace.packages.json"
+
 # Initialize Go workspace
 function init_workspace {
     # Check if go.mod already exists
-    if [ -f go.mod ]; then
+    if [ -f go.work ]; then
         echo "Go workspace already initialized."
     else
         go work init
@@ -24,31 +26,47 @@ function install_common_modules {
     echo "Common modules installed."
 }
 
-# Compare hash of workspace.packages.json
-function compare {
-    local current_hash=$(jq -r '.hash' workspace.packages.json)
-    local calculated_hash=$(jq -c '.packages' workspace.packages.json | sha256sum | awk '{ print $1 }')
+# Function to calculate MD5 hash of a string based on OS
+function calculate_md5 {
+    case "$(uname -s)" in
+        Linux*)     MD5_CMD="md5sum";;
+        Darwin*)    MD5_CMD="md5 -r";;
+        *)          echo "Unsupported OS"; exit 1;;
+    esac
+    echo -n "$1" | $MD5_CMD | awk '{ print $1 }'
+}
 
-    # Check if hash matches
-    if [ "$current_hash" != "$calculated_hash" ]; then
-        echo "Hash mismatch, installing common modules..."
+function update_md5() {
+
+  # Read package names from workspace.packages.json and concatenate them
+  packages=$(jq -r '.packages[]' $PACKAGE_FILE | tr -d '\n')
+
+  # Calculate MD5 hash of the concatenated package names
+  md5_hash=$(calculate_md5 "$packages")
+
+  current_hash=$(jq -r '.hash' $PACKAGE_FILE)
+  if [ -z "$current_hash" ] || [ "$current_hash" != "$md5_hash" ]; then
+        jq --arg md5_hash "$md5_hash" '.hash = $md5_hash' $PACKAGE_FILE > temp.json && mv temp.json $PACKAGE_FILE
+        echo "Hash field set with MD5 hash in $PACKAGE_FILE"
+
         install_common_modules
+  else
+        echo "No existing hash found. Setting new hash value..."
+  fi
 
-        # Update the hash in workspace.packages.json
-        jq --arg new_hash "$calculated_hash" '.hash = $new_hash' workspace.packages.json > temp.json && mv temp.json workspace.packages.json
-        echo "Hash updated in workspace.packages.json"
-    else
-        echo "Hashes match, no need to install common modules."
-    fi
 }
 
 # Main function
 function main {
     # Initialize Go workspace
-    init_workspace
-
-    # Compare and possibly install common modules
-    compare
+    # Check if go.mod already exists
+    if [ -f go.work ]; then
+        echo "Go workspace already initialized."
+    else
+        go work init
+        echo "Go workspace initialized."
+    fi
+    update_md5
 }
 
 # Execute main function
